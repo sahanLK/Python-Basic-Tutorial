2db666fd485ffe7ceecc60a2a30dfcc0cb985ff3
Sahan <kksahan78@gmail.com>
2023-08-17 21:21:39 +0530

    Day 2

---
+++ Day 2/interger.py
@@ -0,0 +1,7 @@
+num = 100
+num_1 = 100
+
+num_2 = '200'
+
+
+print(num + num_1)
---
+++ Day 2/type_conversion.py
@@ -0,0 +1,26 @@
+
+
+# Convert an inter value to a float value
+int_num = 10
+float_num = float(int_num)
+# print(float_num)
+
+# Convert float type into integer
+float = 20.8
+int_num = int(float)
+# print(int_num)
+
+# Convert string to an integer
+num_string = '10'
+int_string = int(num_string)
+# print(type(int_string))
+
+# Convert int to a string
+int_num = 20
+str_num = str(int_num)
+print(type(int_num))
+
+
+
+
+
